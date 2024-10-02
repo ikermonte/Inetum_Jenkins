d4e05a42de1acb3da94f6f079a5efe07244faf0e
Iker Montero <31950385+ikermonte@users.noreply.github.com>
2024-10-02 10:56:26 +0200

    Add files via upload

---
+++ src/test/java/iker/jenkins/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package iker.jenkins.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
